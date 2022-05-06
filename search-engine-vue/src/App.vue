f793e2885a6ff118e8be1512698f0e41f48cdfa2
optimjie <1623287075@qq.com>
2022-05-06 18:42:56 +0800

    完成静态搜索首页

--- search-engine-vue/src/App.vue
+++ search-engine-vue/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
@@ -22,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px; 取消上边界*/
 }
 </style>
---
+++ search-engine-vue/src/components/HomePage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-row type="flex" justify="end">  <!-- 将来放用户登录以及收藏夹的东西 -->
+      <el-popover
+          placement="bottom"
+          title="收藏夹"
+          width="300"
+          trigger="click"
+          content="将来放收藏夹相关内容，实现效果可以参考edge的收藏夹。">
+        <el-button slot="reference" icon="el-icon-folder" circle></el-button>
+      </el-popover>
+      <span>&nbsp;&nbsp;</span>
+      <el-popover
+          placement="bottom"
+          title="用户"
+          width="300"
+          trigger="click"
+          content="将来放关于用户的信息。">
+        <el-button slot="reference" icon="el-icon-user" circle></el-button>
+      </el-popover>
+    </el-row>
+    <el-row id="input">
+      <img src="~@/assets/1057.png" alt="">
+        <el-input v-model="input_value" placeholder="请输入搜索内容" prefix-icon="el-icon-search">
+          <el-button style="margin-right:10px" slot="suffix" type="text" @click="search">搜索</el-button>
+        </el-input>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      input_value: '',
+    }
+  },
+  methods: {
+    search() {
+      // 需要请求数据来显示
+    }
+  }
+};
+</script>
+
+<style scoped>
+  #input {
+    text-align: center; /*让div内部文字居中*/
+    border-radius: 40px;
+    width: 600px;
+    height: 500px;
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+</style>
