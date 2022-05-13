a96a6ee1b999e13be11606d0bf3ca45359c05e12
ingffwe <1170411311@qq.com>
2022-05-13 18:35:04 +0800

    停用词不会再被写入

--- search-engine-spring-boot/src/test/java/com/searchengine/springboot/segmentation/addAllSeg.java
+++ search-engine-spring-boot/src/test/java/com/searchengine/springboot/segmentation/addAllSeg.java
@@ -23,6 +23,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.*;
 
@@ -49,85 +53,124 @@ public class addAllSeg {
     TFIDFAnalyzer tfidfAnalyzer=new TFIDFAnalyzer();
     JiebaSegmenter jiebaSegmenter = new JiebaSegmenter();
 
+    static HashSet<String> stopWordsSet;
+
 
     @Test
     public void addAllSeg(){
 
+        //-----------------初始化-------------
         List<Record> records = recordService.queryAllRecord();
         List<Segmentation> segmentations = segmentationService.queryAllSeg();
 
         BloomFilter<String> bf = BloomFilter.create(Funnels.stringFunnel(Charset.forName("UTF-8")),10000000);
 
+        if(stopWordsSet==null) {
+            stopWordsSet=new HashSet<>();
+            loadStopWords(stopWordsSet, this.getClass().getResourceAsStream("/jieba/stop_words.txt"));
+        }
+
         for (Segmentation seg : segmentations) {
             bf.put(seg.getWord());
         }
+        //----------------初始化结束---------------
 
-        List<String> segs = new ArrayList<>(10000);
-        List<RecordSeg> relations = new ArrayList<>(10000);
 
-        int segMaxId = segmentationDao.getMaxId();  // 获取seg表中最大的id
+        //----------------开始加词-----------------
+        for (int loop=0;loop<100;loop++) {
 
-        for (int i = 0; i < 10000; i++) {  // 10000 15s
-            Record record = records.get(i);
-            String caption = record.getCaption();
-            List<SegToken> segTokens = jiebaSegmenter.process(caption, JiebaSegmenter.SegMode.INDEX);
-            List<Keyword> keywords = tfidfAnalyzer.analyze(caption,5);
-            Map<String,RecordSeg> countMap = new HashMap<>();
-            for (SegToken segToken : segTokens) {
-                String word = segToken.word;
-                int segId = 0;
-                boolean exist = false;
-                if (!bf.mightContain(word)) {  // 不存在是一定不存在
-                    bf.put(word);
-                    segs.add(word);
-                    segId = ++segMaxId;
-                    segmentations.add(new Segmentation(segMaxId, word));
-                } else {  // 但是存在不一定是真的存在，但是这种误报的可能性很小，所以这时全部遍历的时间开销是完全可以接受的。
-                          // https://www.geeksforgeeks.org/bloom-filter-in-java-with-examples/ 误报概率参考，1千万分之一
-                    // 需要检查一下是不是真的存在
-                    for (Segmentation seg : segmentations) {
-                        if (word.equals(seg.getWord())) {
-                            segId = seg.getId();
-                            exist = true;
-                            break;
-                        }
-                    }
-                    if (!exist) {  // 和上面的操作相同
+            List<String> segs = new ArrayList<>(10000);
+            List<RecordSeg> relations = new ArrayList<>(10000);
+
+            int segMaxId = segmentationDao.getMaxId();  // 获取seg表中最大的id
+
+            for (int i = loop*10000; i < (loop+1)*10000; i++) {  // 10000 15s
+                Record record = records.get(i);
+                String caption = record.getCaption();
+                List<SegToken> segTokens = jiebaSegmenter.process(caption, JiebaSegmenter.SegMode.INDEX);
+                List<Keyword> keywords = tfidfAnalyzer.analyze(caption,5);
+                Map<String,RecordSeg> countMap = new HashMap<>();
+                for (SegToken segToken : segTokens) {
+                    String word = segToken.word;
+                    if (stopWordsSet.contains(word)) continue;//判断是否是停用词
+                    int segId = 0;
+                    boolean exist = false;
+                    if (!bf.mightContain(word)) {  // 不存在是一定不存在
                         bf.put(word);
                         segs.add(word);
                         segId = ++segMaxId;
                         segmentations.add(new Segmentation(segMaxId, word));
+                    } else {  // 但是存在不一定是真的存在，但是这种误报的可能性很小，所以这时全部遍历的时间开销是完全可以接受的。
+                              // https://www.geeksforgeeks.org/bloom-filter-in-java-with-examples/ 误报概率参考，1千万分之一
+                        // 需要检查一下是不是真的存在
+                        for (Segmentation seg : segmentations) {
+                            if (word.equals(seg.getWord())) {
+                                segId = seg.getId();
+                                exist = true;
+                                break;
+                            }
+                        }
+                        if (!exist) {  // 和上面的操作相同
+                            bf.put(word);
+                            segs.add(word);
+                            segId = ++segMaxId;
+                            segmentations.add(new Segmentation(segMaxId, word));
+                        }
                     }
-                }
 
-                int dataId = record.getId();
-                double tf = 0;
-                for (Keyword v : keywords) {
-                    if (v.getName().equals(word)) {
-                        tf = v.getTfidfvalue();
-                        break;
+                    int dataId = record.getId();
+                    double tf = 0;
+                    for (Keyword v : keywords) {
+                        if (v.getName().equals(word)) {
+                            tf = v.getTfidfvalue();
+                            break;
+                        }
                     }
+                    //--------------计数--------------
+                    if (!countMap.containsKey(word)){
+                        int count = 1;
+                        countMap.put(word,new RecordSeg(dataId, segId, tf, count));
+                    }else {
+                        RecordSeg t = countMap.get(word);
+                        int count = t.getCount();
+                        t.setCount(++count);
+                        countMap.put(word,t);
+                    }
+                    //--------------------------------
                 }
-                //--------------计数--------------
-                if (!countMap.containsKey(word)){
-                    int count = 1;
-                    countMap.put(word,new RecordSeg(dataId, segId, tf, count));
-                }else {
-                    RecordSeg t = countMap.get(word);
-                    int count = t.getCount();
-                    t.setCount(++count);
-                    countMap.put(word,t);
+                for (RecordSeg t : countMap.values()) {
+                    relations.add(t);
                 }
-                //--------------------------------
             }
-            for (RecordSeg t : countMap.values()) {
-                relations.add(t);
-            }
+
+            segmentationDao.insertBatchSeg(segs);
+            recordSegDao.insertBatch(relations);
         }
 
-        segmentationDao.insertBatchSeg(segs);
-        recordSegDao.insertBatch(relations);
+    }
 
+    private void loadStopWords(Set<String> set, InputStream in){
+        BufferedReader bufr;
+        try
+        {
+            bufr = new BufferedReader(new InputStreamReader(in));
+            String line=null;
+            while((line=bufr.readLine())!=null) {
+                set.add(line.trim());
+            }
+            try
+            {
+                bufr.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
